46fe9adcc3d3cc00927d5dba76ce229e43919b97
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2023-12-31 22:18:12 +0530

    new1

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,23 +1,14 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const {
-  ApplicationCommandOptionType,
-  PermissionFlagsBits,
-} = require('discord.js');
-
-// Rest of your code...
- require('discord.js');
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
   deleted: false,
   name: 'ban',
   description: 'Bans a member!!!',
-  // devOnly: Boolean,
-  // testOnly: Boolean,
   options: [
     {
       name: 'target-user',
       description: 'The user to ban.',
-      // required: true, 
+      required: true,
       type: ApplicationCommandOptionType.Mentionable,
     },
     {
@@ -26,10 +17,33 @@ module.exports = {
       type: ApplicationCommandOptionType.String,
     },
   ],
-  permissionsRequired: [PermissionFlagsBits.Administrator],
-  botPermissions: [PermissionFlagsBits.Administrator],
+  permissionsRequired: [PermissionFlagsBits.BanMembers],
+  botPermissions: [PermissionFlagsBits.BanMembers],
 
-  callback: (client, interaction) => {
-    interaction.reply('ban..');
+  callback: async (client, interaction) => {
+    // Check if the user has the necessary permissions
+    if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers)) {
+      return interaction.reply({
+        content: 'You do not have permission to use this command.',
+        ephemeral: true,
+      });
+    }
+
+    // Extract the target user from the interaction options
+    const targetUser = interaction.options.getMentionable('target-user');
+
+    // Check if a reason was provided
+    const reason = interaction.options.getString('reason') || 'No reason specified';
+
+    try {
+      // Ban the user
+      await interaction.guild.members.ban(targetUser, { reason });
+
+      // Reply to the interaction
+      interaction.reply(`Successfully banned ${targetUser.tag}! Reason: ${reason}`);
+    } catch (error) {
+      console.error(`Error banning user: ${error.message}`);
+      interaction.reply('There was an error banning the user.');
+    }
   },
 };
--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,35 +1,49 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const {
-  ApplicationCommandOptionType,
-  PermissionFlagsBits,
-} = require('discord.js');
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
 
-// Rest of your code...
-
-  module.exports = {
-    deleted: false ,
-    name: 'kick',
-    description: 'kick a member!!!',
-    // devOnly: Boolean,
-    // testOnly: Boolean,
-    options: [
-      {
-        name: 'target-user',
-        description: 'The user to kick.',
-        // required: true,
-        type: ApplicationCommandOptionType.Mentionable,
-      },
-      {
-        name: 'reason',
-        description: 'The reason for banning.',
-        type: ApplicationCommandOptionType.String,
-      },
-    ],
-    permissionsRequired: [PermissionFlagsBits.Administrator],
-    botPermissions: [PermissionFlagsBits.Administrator],
-  
-    callback: (client, interaction) => {
-      interaction.reply('ban..');
+module.exports = {
+  deleted: false,
+  name: 'kick',
+  description: 'Kick a member!!!',
+  options: [
+    {
+      name: 'target-user',
+      description: 'The user to kick.',
+      required: true,
+      type: ApplicationCommandOptionType.Mentionable,
     },
-  };
-  
+    {
+      name: 'reason',
+      description: 'The reason for kicking.',
+      type: ApplicationCommandOptionType.String,
+    },
+  ],
+  permissionsRequired: [PermissionFlagsBits.KickMembers],
+  botPermissions: [PermissionFlagsBits.KickMembers],
+
+  callback: async (client, interaction) => {
+    // Check if the user has the necessary permissions
+    if (!interaction.member.permissions.has(PermissionFlagsBits.KickMembers)) {
+      return interaction.reply({
+        content: 'You do not have permission to use this command.',
+        ephemeral: true,
+      });
+    }
+
+    // Extract the target user from the interaction options
+    const targetUser = interaction.options.getMentionable('target-user');
+
+    // Check if a reason was provided
+    const reason = interaction.options.getString('reason') || 'No reason specified';
+
+    try {
+      // Kick the user
+      await interaction.guild.members.kick(targetUser, { reason });
+
+      // Reply to the interaction
+      interaction.reply(`Successfully kicked ${targetUser.tag}! Reason: ${reason}`);
+    } catch (error) {
+      console.error(`Error kicking user: ${error.message}`);
+      interaction.reply('There was an error kicking the user.');
+    }
+  },
+};
